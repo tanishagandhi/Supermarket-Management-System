fcfbfba96acf86ebf22aa652cfc73b6cd8b4e57c
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-05-02 17:10:13 +0530

    Create Conn.java

---
+++ src/supermarketsystem/Conn.java
@@ -0,0 +1,21 @@
+package supermarketsystem;
+
+import java.sql.*; 
+
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///supermarketmanagementsystem","your sql id"," sql password");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}
